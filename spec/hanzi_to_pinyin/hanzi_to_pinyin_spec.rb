e897297e54b95faeba4a90036d66e2445c16905d
jdd <jdd@netposa.com>
2013-06-19 11:22:28 +0800

    hztpy.json: 常用字去掉生僻读音

--- spec/hanzi_to_pinyin/hanzi_to_pinyin_spec.rb
+++ spec/hanzi_to_pinyin/hanzi_to_pinyin_spec.rb
@@ -21,16 +21,16 @@ describe HanziToPinyin do
   it "hanzi_2_py method" do
     HanziToPinyin.hanzi_2_py("我们").should == "wo;men"
     HanziToPinyin.hanzi_2_py("yyf").should == "yyf"
-    HanziToPinyin.hanzi_2_py("查理Smith").should == "cha,zha;li;Smith"
+    HanziToPinyin.hanzi_2_py("查理Smith").should == "cha;li;Smith"
     HanziToPinyin.hanzi_2_py("你你").should == "ni;ni"
-    HanziToPinyin.hanzi_2_py("郭轶").should == "guo;yi,die"
-    HanziToPinyin.hanzi_2_py("宗志强").should == "zong;zhi;qiang,jiang"
+    HanziToPinyin.hanzi_2_py("郭轶").should == "guo;yi"
+    HanziToPinyin.hanzi_2_py("宗志强").should == "zong;zhi;qiang"
     HanziToPinyin.hanzi_2_py("测试1").should == "ce;shi;1"
     HanziToPinyin.hanzi_2_py("2测试").should == "2;ce;shi"
     HanziToPinyin.hanzi_2_py("测3试").should == "ce;3;shi"
     HanziToPinyin.hanzi_2_py("测_试").should == "ce;_;shi"
     HanziToPinyin.hanzi_2_py("测-试").should == "ce;-;shi"
-    HanziToPinyin.hanzi_2_py("金龙—技术支持").should == "jin;long,mang;—;ji;zhu,shu;zhi;chi"
+    HanziToPinyin.hanzi_2_py("金龙—技术支持").should == "jin;long;—;ji;shu;zhi;chi"
     HanziToPinyin.hanzi_2_py(2).should == "2"
   end
   
@@ -56,4 +56,19 @@ describe HanziToPinyin do
     HanziToPinyin.is_dash?("_".ord).should be_false
   end
   
+  it "常用字去掉生僻读音" do
+    HanziToPinyin.hanzi_2_py("豉").should == "chi"
+    HanziToPinyin.hanzi_2_py("空调").should == "kong;diao,tiao"
+    HanziToPinyin.hanzi_2_py("帝都").should == "di;du,dou"
+    HanziToPinyin.hanzi_2_py("哦").should == "e,o"
+    HanziToPinyin.hanzi_2_py("可恶").should == "ke;e,wu"
+    HanziToPinyin.hanzi_2_py("吐鲁番").should == "tu;lu;fan,pan" 
+    HanziToPinyin.hanzi_2_py("仿佛").should == "fang;fo,fu"
+    HanziToPinyin.hanzi_2_py("乾隆").should == "qian;long"
+    HanziToPinyin.hanzi_2_py("信息部").should == "xin;xi;bu"
+    HanziToPinyin.hanzi_2_py("蜈蚣").should == "wu;gong"
+    HanziToPinyin.hanzi_2_py("朝阳门").should ==  "zhao,chao;yang;men"
+    HanziToPinyin.hanzi_2_py("女人").should == "nÜ;ren" 
+    HanziToPinyin.hanzi_2_py("研发中心").should == "yan;fa;zhong;xin"
+  end
 end
